88436315316a260368bee42f6a15cc85bd43a790
Bomb Atomically <ieugenelee@gmail.com>
2021-07-15 11:27:53 -0700

    save textarea to localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -3,5 +3,6 @@ $(".btn").on("click",function(event){ // add event click listener -- specifies w
     var hour = $(this).attr("id").split("-")[0] // targets only the id, split id into '12 | savebtn', target first elemnt [0] = 12
     var usePlan = $("#"+hour + "-planner").val() // generating a new id with the corresponding textarea and grabbing the value
     console.log(hour,usePlan)
+    localStorage.setItem(hour,usePlan)
 });
 
