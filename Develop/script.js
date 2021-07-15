07e964ede509e97d86eede5c4a429d45dddc49a4
Bomb Atomically <ieugenelee@gmail.com>
2021-07-15 11:31:19 -0700

    able to retrieve from localstorage in text block

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,9 @@ $(".btn").on("click",function(event){ // add event click listener -- specifies w
     var hour = $(this).attr("id").split("-")[0] // targets only the id, split id into '12 | savebtn', target first elemnt [0] = 12
     var usePlan = $("#"+hour + "-planner").val() // generating a new id with the corresponding textarea and grabbing the value
     console.log(hour,usePlan)
-    localStorage.setItem(hour,usePlan)
+    localStorage.setItem(hour,usePlan) // added localStorage
 });
 
+for(let i=9;i<=17;i++){ // create for loop to display localStorage from textarea after refreshing
+    $("#"+i+"-planner").val(localStorage.getItem(i)) // targets each textarea based on #id-planner
+}
