44131b48032efcf2c269a56632223d3b0243c32f
Bomb Atomically <ieugenelee@gmail.com>
2021-07-15 11:26:07 -0700

    achieve functionality to grab user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
 $(".btn").on("click",function(event){ // add event click listener -- specifies which btn is clicked
     event.preventDefault() // good input to have
-    var hour = $(this).attr("id") // targets only the id
-    console.log(hour)
-})
+    var hour = $(this).attr("id").split("-")[0] // targets only the id, split id into '12 | savebtn', target first elemnt [0] = 12
+    var usePlan = $("#"+hour + "-planner").val() // generating a new id with the corresponding textarea and grabbing the value
+    console.log(hour,usePlan)
+});
+
